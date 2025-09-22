f4768baf55cb5bfc66c7d9bbc90a665be6da1385
Frank McCrudden <fmccrudden@cps.edu>
2025-09-22 10:35:59 -0500

    day 2 i think

--- a2.py
+++ a2.py
@@ -27,28 +27,44 @@ def match(pattern: List[str], source: List[str]) -> List[str]:
         # you should delete the following line
 
         # 1) if we reached the end of the pattern but not source
-        if len(pattern) < pind and len(source) < sind:
+        if pind == len(pattern) and sind < len(sind):
+            print("End of pattern, but not source")
+            return None
         
         # 2) if the current thing in the pattern is a %
         # WARNING: this condition contains the bulk of the code for the assignment
         # If you get stuck on this one, we encourage you to attempt the other conditions
         #   and come back to this one afterwards
-
+        elif pattern[pind] == "%":
+            if pind == len(pattern) - 1:
+                combined = " ".join(source[sind:])
+                result.append(combined)
+                print (result)
+                return result
+            else:
+                pass
         # 3) if we reached the end of the source but not the pattern
-
+        elif sind == len(source):
+            print("End of source, but not pattern")
+            return None
         # 4) if the current thing in the pattern is an _
-
+        elif pattern[pind] == "_":
+            result.append(source[sind])
+            print(result)
+            pind += 1
+            sind += 1
         # 5) if the current thing in the pattern is the same as the current thing in the
         # source
-            if pattern[pind] == source[sind]:
-                print(pattern[pind], source[sind])
-                pind += 1
-                sind += 1
+        elif pattern[pind] == source[sind]:
+            print(pattern[pind], source[sind])
+            pind += 1
+            sind += 1
 
         # 6) else : this will happen if none of the other conditions are met it
         # indicates the current thing it pattern doesn't match the current thing in
         # source
         else:
+            print(pattern[pind], source[sind])
             print("End of assert, return none")
             return None
     print("End of assert, return list")
